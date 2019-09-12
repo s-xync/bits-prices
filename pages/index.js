1c0b964fe383d3d8b6250932692ff2362adede9f
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-09-12 22:59:22 +0530

    done with the next js crash course

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
+import Fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
+import Prices from "../components/Prices";
 
-const Index = () => (
+const Index = props => (
   <Layout>
     <div>
       <h1>Welcome to Bits Price</h1>
+      <p>Check current Bitcoin rate</p>
+      <Prices bpi={props.bpi} />
     </div>
   </Layout>
 );
+
+Index.getInitialProps = async () => {
+  const res = await Fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const data = await res.json();
+
+  return {
+    bpi: data.bpi
+  };
+};
 
 export default Index;
